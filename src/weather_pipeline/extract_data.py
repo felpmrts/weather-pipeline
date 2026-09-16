d7fda7711be992baf44e5178b321611b7c3b414d
felpmrts <felipe_martins__@outlook.com>
2026-09-16 12:03:46 -0300

    feat: add logging to weather data extraction

--- src/weather_pipeline/extract_data.py
+++ src/weather_pipeline/extract_data.py
@@ -2,18 +2,21 @@ import requests
 import json
 from pathlib import Path
 
+import logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
 def extract_weather_data(url:str) -> list:
      response = requests.get(url)
      
 
      if response.status_code != 200:
-          print("Erro de requisição")
+          logging.error("Erro de requisição")
           return []
      
      data = response.json()
      
      if not data:
-          print("Nenhum dado retornado")
+          logging.warning("Nenhum dado retornado")
           return []
 
      output_path = 'data/weather_data.json'
@@ -23,5 +26,5 @@ def extract_weather_data(url:str) -> list:
      with open(output_path, 'w') as f:
           json.dump(data,f,indent=4)
 
-     print(f"Arquivo salvo em {output_path}")
+     logging.info(f"Arquivo salvo em {output_path}")
      return data
